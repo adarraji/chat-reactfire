3f0e546e7fa86d7efda6c9e7c849fcbb861315c5
adarraji <a.darraji@outlook.com>
2023-05-24 11:44:23 -0400

    added FirebaseAppProvider and firebase config to index

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,8 @@
 import "./App.css";
 import { doc, getFirestore } from 'firebase/firestore';
-import { FirebaseAppProvider, FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
+import { FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
 
 
-const firebaseConfig =
-{
-  apiKey: "AIzaSyCDWLvWFGopxBt_w6hFKBcNtxR23kwOvwI",
-  authDomain: "chat-64fb5.firebaseapp.com",
-  projectId: "chat-64fb5",
-  storageBucket: "chat-64fb5.appspot.com",
-  messagingSenderId: "321175385761",
-  appId: "1:321175385761:web:e48b81fe19d2e98b17e2e0"
-};
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import { FirebaseAppProvider } from 'reactfire';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const firebaseConfig =
+{
+  apiKey: "AIzaSyCDWLvWFGopxBt_w6hFKBcNtxR23kwOvwI",
+  authDomain: "chat-64fb5.firebaseapp.com",
+  projectId: "chat-64fb5",
+  storageBucket: "chat-64fb5.appspot.com",
+  messagingSenderId: "321175385761",
+  appId: "1:321175385761:web:e48b81fe19d2e98b17e2e0"
+};
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider>
+      <App />
+    </FirebaseAppProvider>
   </React.StrictMode>
 );
 
