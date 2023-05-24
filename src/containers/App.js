d06de6323e37009fd4da1f48a87acd01641c6699
adarraji <a.darraji@outlook.com>
2023-05-24 12:14:09 -0400

    fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <AuthProvider sdk={auth}>
-        <DatabaseProvider>
+        <DatabaseProvider sdk={database}>
           <SignIn />
         </DatabaseProvider>
       </AuthProvider>
