958ccd15aabe20531f6bd19ac43e712d77d60443
adarraji <a.darraji@outlook.com>
2023-05-24 12:12:02 -0400

    added signin to app

--- src/components/signIn/SignIn.jsx
+++ src/components/signIn/SignIn.jsx
@@ -1,9 +1,20 @@
-import React from "react"
+import React from "react";
+import { useAuth } from 'reactfire';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
+    const auth = useAuth();
+    const signInWithGoogle = async () => {
+        const provider = new GoogleAuthProvider();
+        await signInWithPopup(auth, provider);
+    }
+
+    return (
+        <>
+            <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+
+        </>
+    )
 }
 
 export default SignIn
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
 import { doc, getFirestore } from 'firebase/firestore';
-import { FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
-
-
-
+import { FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp, useAuth, AuthProvider } from 'reactfire';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import SignIn from "../components/signIn/SignIn";
 
 
 
 
 function App() {
+  const app = useFirebaseApp();
+  const database = getDatabase(app);
+  const auth = getAuth(app);
+
   return (
     <div className="App">
-      App
+      <AuthProvider sdk={auth}>
+        <SignIn />
+      </AuthProvider>
     </div>
   );
 }
