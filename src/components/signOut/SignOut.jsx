4299b3775a3a55d17749fb3584650da5bf92f98f
adarraji <a.darraji@outlook.com>
2023-05-24 13:08:41 -0400

    added signout to app

--- src/components/signOut/SignOut.jsx
+++ src/components/signOut/SignOut.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import { useAuth } from 'reactfire';
 
-const signOut = () => {
-  return (
-    <div>signOut</div>
-  )
+const SignOut = () => {
+    const auth = useAuth();
+    return (
+        <div>
+            <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+        </div>
+    )
 }
 
-export default signOut
+export default SignOut
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,14 +1,17 @@
 import React from "react"
-import { useSigninCheck } from 'reactfire';
+import { useAuth, useSigninCheck } from 'reactfire';
 import SignIn from "../components/signIn/SignIn"
+import SignOut from "../components/signOut/SignOut"
+
 
 const Home = () => {
-    // const auth = useAuth();
-    // const { status, data: signInCheckResult } = useSigninCheck();
-    // console.log(signInCheckResult);
+    const auth = useAuth();
+    const { status, data: signInCheckResult } = useSigninCheck();
+    console.log(signInCheckResult);
     return (
         <div>
             <SignIn />
+            <SignOut />
         </div>
     )
 }
