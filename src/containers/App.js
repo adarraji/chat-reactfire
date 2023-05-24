0b27810d226b4901c39196f4471b4a540fc5ef3a
adarraji <a.darraji@outlook.com>
2023-05-24 11:21:58 -0400

    added firebase config

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,19 @@ import { doc, getFirestore } from 'firebase/firestore';
 import { FirebaseAppProvider, FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
 
 
+const firebaseConfig =
+{
+  apiKey: "AIzaSyCDWLvWFGopxBt_w6hFKBcNtxR23kwOvwI",
+  authDomain: "chat-64fb5.firebaseapp.com",
+  projectId: "chat-64fb5",
+  storageBucket: "chat-64fb5.appspot.com",
+  messagingSenderId: "321175385761",
+  appId: "1:321175385761:web:e48b81fe19d2e98b17e2e0"
+};
+
+
+
+
 
 function App() {
   return (
