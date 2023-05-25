90c9952c3bac71f497466ebc3986d2eb4b48b098
adarraji <a.darraji@outlook.com>
2023-05-25 07:10:38 -0400

    added message

--- src/components/signIn/SignIn.jsx
+++ src/components/signIn/SignIn.jsx
@@ -1,3 +1,4 @@
+import "./signIn.css";
 import React from "react";
 import { useAuth, useUser } from "reactfire";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
@@ -13,6 +14,7 @@ const SignIn = () => {
     return (
         <>
             <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+            <p>Do not violate the community guidelines or you will be banned for life!</p>
         </>
     )
 }
